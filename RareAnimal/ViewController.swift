58e9fc83de7b847b3df09b6e4b8193160423fd5b
student3 <student3@jh-mbair-158-14.local>
2016-05-02 14:04:31 -0500

    Initial Commit

---
+++ RareAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RareAnimal
+//
+//  Created by student3 on 5/2/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
