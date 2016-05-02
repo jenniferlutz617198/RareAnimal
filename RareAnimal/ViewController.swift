3be8e590a005a6e21bb09cf18de68d91c86a9caa
student3 <student3@jh-mbair-158-14.d214.org>
2016-05-02 14:43:05 -0500

    added action sheet

--- RareAnimal/ViewController.swift
+++ RareAnimal/ViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var picker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
     @IBAction func onTapped(sender: UIButton) {
+        let actionsheet = UIAlertController(title: "Select image", message: nil, preferredStyle: .ActionSheet)
+        actionsheet.popoverPresentationController?.sourceView = self.view
+        actionsheet.popoverPresentationController?.sourceRect = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: 100)
+        let cameraButton = UIAlertAction(title: "camera", style: .Default) { (action) in
+            self.picker.sourceType = UIImagePickerControllerSourceType.Camera
+            self.presentViewController(self.picker, animated: true, completion: nil)
+        }
+        actionsheet.addAction(cameraButton)
+        let libraryButton = UIAlertAction(title: "library", style: .Default) { (action) in
+            self.picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(self.picker, animated: true, completion: nil)
+        }
+        actionsheet.addAction(libraryButton)
+        presentViewController(actionsheet, animated: true, completion: nil)
         
     }
-
+    
 }
 
